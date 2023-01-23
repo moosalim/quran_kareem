bd56da88a99562e5ba1c63409e425f4f934a404c
moosalim <moosalim@gmail.com>
2023-01-23 20:16:31 +0400

    fix dart version

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ytquran/constant.dart';
 import 'index.dart';
+
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,20 +18,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   void initState() {
-    WidgetsBinding
-        .instance
-        .addPostFrameCallback(
-
-            (_) async{
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
       await readJson();
       await getSettings();
-    }
-
-
-
-    );
+    });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
